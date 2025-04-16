8158e7294f5ac8fa4063719ea345d415f375afd7
Tomik <maxiwrayt@gmail.com>
2025-04-16 15:57:55 +0200

    feat(notes): add note form and creation logic in views

--- notes/views.py
+++ notes/views.py
@@ -1,4 +1,23 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render
 
+from .forms import NoteForm
+
+
+@login_required(login_url="/user/sign-in")
 def create_note(request):
-    return render(request, "notes/create-note.html")
+    if request.method == "POST":
+        form = NoteForm(request.POST)
+        if form.is_valid():
+            note = form.save(commit=False)
+            note.user = request.user
+            note.save()
+            return redirect("core:index")
+    else:
+        form = NoteForm()
+
+    context = {
+        "title": "Create Note - Notes",
+        "form": form,
+    }
+    return render(request, "notes/create-note.html", context)
