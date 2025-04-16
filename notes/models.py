846476048d58ccbc5b30c025265807453256d4f4
Tomik <maxiwrayt@gmail.com>
2025-04-16 13:22:55 +0200

    feat(notes): define Note model

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+from users.models import User
+
+
+class Note(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=60)
+    content = models.TextField()
+    is_public = models.BooleanField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "note"
