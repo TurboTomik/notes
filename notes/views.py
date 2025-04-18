6e844fb3a67c411cc97ed31070b57f32d00e6249
Tomik <maxiwrayt@gmail.com>
2025-04-18 12:14:03 +0200

    fix(notes): add title in my-notes page

--- notes/views.py
+++ notes/views.py
@@ -24,11 +24,12 @@ def create_note(request):
     }
     return render(request, "notes/create-note.html", context)
 
+
 @login_required(login_url="/user/sign-in")
 def my_notes(request):
     notes = Note.objects.filter(user_id=request.user.id)
     context = {
-        "notes": notes
+        "title": "My Notes - Notes",
+        "notes": notes,
     }
     return render(request, "notes/my-notes.html", context)
-    
