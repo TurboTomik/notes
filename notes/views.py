e6a859980b8ce9d431373237d2c1bffa9c4ad013
Tomik <maxiwrayt@gmail.com>
2025-04-19 10:27:51 +0200

    feat(notes): add edit-note view

--- notes/views.py
+++ notes/views.py
@@ -1,9 +1,9 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 
 from notes.models import Note
 
-from .forms import NoteForm
+from .forms import NoteForm, EditNoteForm
 
 
 @login_required(login_url="/user/sign-in")
@@ -37,7 +37,17 @@ def my_notes(request):
 
 @login_required(login_url="/user/sign-in")
 def edit_note(request, note_id):
+    note = get_object_or_404(Note, pk=note_id, user_id=request.user.id)
+    if request.method == "POST":
+        form = EditNoteForm(data=request.POST, instance=note)
+        if form.is_valid():
+            form.save()
+            return redirect("notes:edit-note", note_id)
+    else:
+        form = EditNoteForm(instance=note)
+
     context = {
         "title": "Edit Note - Notes",
+        "form": form,
     }
     return render(request, "notes/edit-note.html", context)
