80221bb9eaec96a5c64d81c0d7e4822338f6673a
Tomik <maxiwrayt@gmail.com>
2025-04-21 19:29:15 +0200

    refactor(all apps): add block header in base.html

--- core/views.py
+++ core/views.py
@@ -3,5 +3,6 @@ from django.shortcuts import render
 def index(request):
     context = {
         "title": "Home - Notes",
+        "index_page": True,
     }
     return render(request, "core/index.html", context)
